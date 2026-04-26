1d1aa323cb50158ec6577e16199e09f9fa4cfa14
Jacks Diao <kdha200501@gmail.com>
2026-04-26 15:21:42 -0400

    master: fix import

--- service/process.js
+++ service/process.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { readFile } = require('fs/promises');
-const { findProcess } = require('find-process/lib/find_process');
+const find = require('find-process');
 
 /**
  * check if a process with the given PID is still running
@@ -12,7 +12,7 @@ const isPreviousInstanceRunning = (lockFilePath) =>
   readFile(lockFilePath, 'utf8')
     .then(JSON.parse)
     .then((/** @type {LockFileContent} */ lockFileContent) =>
-      findProcess({ pid: lockFileContent.pid })
+      find('pid', lockFileContent.pid)
     ) /** @param {unknown[]} results */
     .then((results) => !!results[0]);
 
